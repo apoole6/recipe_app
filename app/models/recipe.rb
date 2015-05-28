1574a12a783ce2a6df22a162e3dad0cdc0abacd5
Austin Poole <austinpoole@Austins-MacBook-Pro.local>
2015-05-27 20:56:12 -0400

    update 2

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,16 @@
 class Recipe < ActiveRecord::Base
+
+	validates :name, presence: true, uniqueness: true, length: {minimum: 2}
+	validates :category, presence: true, uniqueness: true, length: {minimum: 2}
+	validates :instructions, presence: true, uniqueness: true, length: {maximum: 1500}
+	validates :minutes_to_prepare, presence: true, uniqueness: true, numericality: {greater_than: -1}
+	validates :minutes_to_cook, presence: true, uniqueness: true, numericality: {greater_than: 0, less_than: 500}
+
+     mount_uploader :picture, PictureUploader 
+	
 	def minutes_total
-		if (minutes_to_prepare)
-			minutes_to_prepare + minutes_to_cook
+		if (minutes_to_prepare > 0)
+			 minutes_to_prepare + minutes_to_cook
 		else
 			minutes_to_cook
 		end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527231240) do
+ActiveRecord::Schema.define(version: 20150528002204) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150527231240) do
     t.integer  "minutes_to_cook"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
+    t.string   "picture"
   end
 
 end
